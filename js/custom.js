b0a2377c1b3c490425ca135e66dbacd3864d6a26
Liloudini Aziz <zizoa1er@gmail.com>
2017-09-12 09:17:10 +0200

    Modification de quelques termes anglais.

--- js/custom.js
+++ js/custom.js
@@ -1,53 +1,52 @@
 (function ($) {
- "use strict";
-$(document).ready(function(){
-	  
+	"use strict";
+	$(document).ready(function(){
+
 		/*
-		Mean Menu Responsive
-		============================*/		
-        jQuery('nav#main-menu').meanmenu();						
+		 Mean Menu Responsive
+		 ============================*/
+		jQuery('nav#main-menu').meanmenu();
 		/*
-		Testimonial Crousel
-		============================*/ 
-		
-		  $(".all-testimonial").owlCarousel({
-			autoPlay: false, 
+		 Testimonial Crousel
+		 ============================*/
+
+		$(".all-testimonial").owlCarousel({
+			autoPlay: false,
 			slideSpeed:2000,
 			pagination:false,
-			navigation:true, 
+			navigation:true,
 			items :1,
 			navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
 			itemsDesktop : [1199,1],
 			itemsDesktopSmall : [980,1],
 			itemsTablet: [768,1],
-			itemsTablet: [767,1],
 			itemsMobile : [479,1],
-		  });
-		  
+		});
+
 		/*
-		Patner Crousel
-		============================*/ 
-		  $(".all-patner").owlCarousel({
-			autoPlay: false, 
+		 Patner Crousel
+		 ============================*/
+		$(".all-patner").owlCarousel({
+			autoPlay: false,
 			slideSpeed:2000,
 			pagination:false,
-			navigation:true, 
+			navigation:true,
 			items : 6,
 			navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
 			itemsDesktop : [1199,6],
 			itemsDesktopSmall : [980,5],
 			itemsTablet: [768,5],
 			itemsMobile : [479,3],
-		  });			
-		
+		});
+
 		/*
-		Slider Crousel
-		============================*/ 
-		  $(".all-slide").owlCarousel({
-			autoPlay: false, 
+		 Slider Crousel
+		 ============================*/
+		$(".all-slide").owlCarousel({
+			autoPlay: false,
 			slideSpeed:1500,
 			pagination:false,
-			navigation:true, 
+			navigation:true,
 			mouseDrag: false,
 			items :1,
 			navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
@@ -55,34 +54,34 @@ $(document).ready(function(){
 			itemsDesktopSmall : [980,1],
 			itemsTablet: [768,1],
 			itemsMobile : [479,1],
-		  });
-	  
-		  
+		});
+
+
 		/*
-		scrollUp
-		============================*/	
+		 scrollUp
+		 ============================*/
 		$.scrollUp({
 			scrollText: '<i class="fa fa-angle-up"></i>',
 			easingType: 'linear',
 			scrollSpeed: 900,
 			animation: 'fade'
-		});	
+		});
 		/*
-		Counter Js
-		============================*/ 
-        $('.counter').counterUp({
-            delay: 10,
-            time: 1000			
-        }); 					
+		 Counter Js
+		 ============================*/
+		$('.counter').counterUp({
+			delay: 10,
+			time: 1000
+		});
 		/*
-		Preeloader
-		============================*/
+		 Preeloader
+		 ============================*/
 		$(window).load(function() {
 			$('#preloader').fadeOut();
 			$('#preloader-status').delay(200).fadeOut('slow');
 			$('body').delay(200).css({'overflow-x':'hidden'});
 		});
-		
-	});	
+
+	});
 })(jQuery);
 
